c794dea7d34615d8a7ed68d60d25947ee9f71427
marcinmuller <mmuller@o2.pl>
2017-10-18 05:25:55 +0200

    mod 19.3

---
+++ src/main/java/com/crud/tasks/service/TrelloService.java
@@ -0,0 +1,41 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.*;
+import com.crud.tasks.trello.client.TrelloClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TrelloService {
+
+    public static final String SUBJECT = "new trello card";
+
+    @Autowired
+    private AdminConfig adminConfig;
+    @Autowired
+    private TrelloClient trelloClient;
+
+    @Autowired
+    private SimpleEmailService emailService;
+
+    public List<TrelloBoardDto> fetchTrelloBoards(){
+        return trelloClient.getTrelloBoards();
+    }
+
+    public CreatedTrelloCard createdTrelloCard(final TrelloCardDto trelloCardDto){
+        CreatedTrelloCard newCard = trelloClient.createNewCard(trelloCardDto);
+
+        Optional.ofNullable(newCard).ifPresent(c->
+        emailService.send(new Mail(
+                adminConfig.getAdminMail(),
+                SUBJECT,
+                "new card:"+trelloCardDto.getName()+"has been created"
+                ,"z@zz.z"
+        )));
+        return newCard;
+    }
+}
---
+++ src/test/java/com/crud/tasks/service/SimpleEmailServiceTest.java
@@ -0,0 +1,60 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.domain.Mail;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SimpleEmailServiceTest {
+    @InjectMocks
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private JavaMailSender javaMailSender;
+
+    @Test
+    public void shouldSendEmailWithoutCC(){
+        ///given
+        Mail mail = new Mail("ss@ss.s", "test", "test test", null);
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        mailMessage.setCc(mail.getToCc());
+
+        //when
+        simpleEmailService.send(mail);
+
+        //then
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+
+    @Test
+    public void shouldSendEmailWithCC(){
+        ///given
+        Mail mail = new Mail("flaszowiec@gmail.com", "test", "test test", "ss@ss.ss");
+
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+        mailMessage.setTo(mail.getMailTo());
+        mailMessage.setSubject(mail.getSubject());
+        mailMessage.setText(mail.getMessage());
+        mailMessage.setCc(mail.getToCc());
+
+        //when
+        simpleEmailService.send(mail);
+
+        //then
+        verify(javaMailSender, times(1)).send(mailMessage);
+    }
+}
